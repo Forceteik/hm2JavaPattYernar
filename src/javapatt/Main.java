480d2640e23a7a76796d9f0778221010c75b4dbb
Forceteik <80638590+Forceteik@users.noreply.github.com>
2021-09-28 00:46:02 +0600

    Update Main.java

--- src/javapatt/Main.java
+++ src/javapatt/Main.java
@@ -4,13 +4,19 @@ public class Main {
     public static void main(String[] args) {
         GenshinImpactNotif notif = new GenshinImpactNotif();
 
-        notif.addCharacter("Nin Guan");
-        notif.addCharacter("Qi Qi");
-
         Observer firstPlayer = new Player("Forceteik");
         Observer secondPlayer = new Player("Miruna");
         Observer thirdPlayer = new Player("Imuran");
 
+        notif.addObserver(firstPlayer);
+        notif.addObserver(secondPlayer);
+        notif.addObserver(thirdPlayer);
+
+
+        notif.addCharacter("Ning Guang");
+        notif.addCharacter("Qi Qi");
         notif.addCharacter("Emilia");
+
+        notif.removeCharacter("Emilia");
     }
 }
